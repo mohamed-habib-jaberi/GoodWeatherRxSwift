b91bd25c06ed5c0c3c5bef437e1aba32f1a00b63
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-07 01:13:08 +0200

    Throwing Errors

--- GoodWeatherRxSwift/Extensions/URLRequest+Extensions.swift
+++ GoodWeatherRxSwift/Extensions/URLRequest+Extensions.swift
@@ -17,18 +17,40 @@ struct Ressource<T> {
 
 extension URLRequest {
     
+    // implementing load function that take in consideration the status code that return
+    
     static func load<T: Decodable>(ressource: Ressource<T>) -> Observable<T>{
         
-        return Observable.from([ressource.url])
-            .flatMap { (url) -> Observable<Data> in
+        return Observable.just(ressource.url)
+            .flatMap { (url) -> Observable<(response: HTTPURLResponse, data: Data)> in
                 
                 let request = URLRequest(url: url)
-                return URLSession.shared.rx.data(request: request)
-        }.map { (data) -> T in
+                return URLSession.shared.rx.response(request: request)
+                
+        }.map { response, data -> T in
             
-            try JSONDecoder().decode(T.self, from: data)
+            if 200..<300 ~= response.statusCode {
+                return   try JSONDecoder().decode(T.self, from: data)
+            }else {
+                throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+            }
+            
         }.asObservable()
         
     }
+    
+    //    static func load<T: Decodable>(ressource: Ressource<T>) -> Observable<T>{
+    //
+    //        return Observable.from([ressource.url])
+    //            .flatMap { (url) -> Observable<Data> in
+    //
+    //                let request = URLRequest(url: url)
+    //                return URLSession.shared.rx.data(request: request)
+    //        }.map { (data) -> T in
+    //
+    //            try JSONDecoder().decode(T.self, from: data)
+    //        }.asObservable()
+    //
+    //    }
     
 }
