d017a992b3ebf94bcbb771c4f0f922dee5841610
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-06 15:11:41 +0200

    Building User Interface for the App

--- GoodWeatherRxSwift/ViewController.swift
+++ GoodWeatherRxSwift/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
